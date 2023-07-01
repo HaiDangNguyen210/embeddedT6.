81f001c651e800dbb9c23f6d190163f04cc5d492
HaiDangNguyen210 <136185469+HaiDangNguyen210@users.noreply.github.com>
2023-07-01 18:58:35 +0700

    Delete CodeCpp directory

--- CodeCpp/B6 Phan Vung Nho/text.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-const int i = 10; 
-
-int main(){
-    printf("i=%d",i);
-    // gia tri cua i khong the thay doi duoc
-}
--- CodeCpp/vonglapfor/for.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include<stdint.h>
-int main(){
-    for(uint8_t i = 0; i<10; i++ ){
-        printf("i=%d\n",i);
-    }
-return 0;
-}
